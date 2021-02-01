791a258bebc2983a8558b8d6faa6e888dfa880b3
Prashansa Bhattacharyya <34596426+prashansanb@users.noreply.github.com>
2021-02-01 18:39:44 +0530

    Update token_swapper.py

--- token_swapper.py
+++ token_swapper.py
@@ -1,51 +1,314 @@
 
 import smartpy as sp
 
-def call(c, x):
-    sp.transfer(x, sp.mutez(0), c)
+class Token_X_core(sp.Contract):
+    def __init__(self, **extra_storage):
+        self.init(balances = sp.big_map(tvalue = sp.TRecord(approvals = sp.TMap(sp.TAddress, sp.TNat), balance = sp.TNat)), totalSupply = 0, **extra_storage)
 
-class Token_X(sp.Contract):
-#add contents of token_X smart contract
     @sp.entry_point
-    
+    def transfer(self, params):
+        sp.set_type(params, sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, value = sp.TNat).layout(("from_ as from", ("to_ as to", "value"))))
+        sp.verify(self.is_administrator(sp.sender) | (~self.is_paused() & ((params.from_ == sp.sender) | (self.data.balances[params.from_].approvals[params.from_] >= params.value))))
+        self.addAddressIfNecessary(params.to_)
+        sp.verify(self.data.balances[params.from_].balance >= params.value)
+        self.data.balances[params.from_].balance = sp.as_nat(self.data.balances[params.from_].balance - params.value)
+        self.data.balances[params.to_].balance += params.value
+        sp.if (params.from_ != sp.sender) & (~self.is_administrator(sp.sender)):
+            self.data.balances[params.from_].approvals[params.from_] = sp.as_nat(self.data.balances[params.from_].approvals[params.from_] - params.value)
 
-class Token_Y(sp.Contract):
     @sp.entry_point
-#add contents of token_Y
+    def approve(self, params):
+        sp.set_type(params, sp.TRecord(spender = sp.TAddress, value = sp.TNat).layout(("spender", "value")))
+        sp.verify(~self.is_paused())
+        alreadyApproved = self.data.balances[sp.sender].approvals.get(params.spender, 0)
+        sp.verify((alreadyApproved == 0) | (params.value == 0), "UnsafeAllowanceChange")
+        self.data.balances[sp.sender].approvals[params.spender] = params.value
+
+    def addAddressIfNecessary(self, address):
+        sp.if ~ self.data.balances.contains(address):
+            self.data.balances[address] = sp.record(balance = 0, approvals = {})
+
+    @sp.view(sp.TNat)
+    def getBalance(self, params):
+        sp.result(self.data.balances[params].balance)
+
+    @sp.view(sp.TNat)
+    def getAllowance(self, params):
+        sp.result(self.data.balances[params.owner].approvals[params.spender])
+
+    @sp.view(sp.TNat)
+    def getTotalSupply(self, params):
+        sp.set_type(params, sp.TUnit)
+        sp.result(self.data.totalSupply)
+
+    # this is not part of the standard but can be supported through inheritance.
+    def is_paused(self):
+        return sp.bool(False)
+
+    # this is not part of the standard but can be supported through inheritance.
+    def is_administrator(self, sender):
+        return sp.bool(False)
+
+class Token_X_mint_burn(Token_X_core):
+    @sp.entry_point
+    def mint(self, params):
+        sp.set_type(params, sp.TRecord(address = sp.TAddress, value = sp.TNat))
+        sp.verify(self.is_administrator(sp.sender))
+        self.addAddressIfNecessary(params.address)
+        self.data.balances[params.address].balance += params.value
+        self.data.totalSupply += params.value
+
+    @sp.entry_point
+    def burn(self, params):
+        sp.set_type(params, sp.TRecord(address = sp.TAddress, value = sp.TNat))
+        sp.verify(self.is_administrator(sp.sender))
+        sp.verify(self.data.balances[params.address].balance >= params.value)
+        self.data.balances[params.address].balance = sp.as_nat(self.data.balances[params.address].balance - params.value)
+        self.data.totalSupply = sp.as_nat(self.data.totalSupply - params.value)
+
+class Token_X_administrator(Token_X_core):
+    def is_administrator(self, sender):
+        return sender == self.data.administrator
+
+    @sp.entry_point
+    def setAdministrator(self, params):
+        sp.set_type(params, sp.TAddress)
+        sp.verify(self.is_administrator(sp.sender))
+        self.data.administrator = params
+
+    @sp.view(sp.TAddress)
+    def getAdministrator(self, params):
+        sp.set_type(params, sp.TUnit)
+        sp.result(self.data.administrator)
+
+class Token_X_pause(Token_X_core):
+    def is_paused(self):
+        return self.data.paused
+
+    @sp.entry_point
+    def setPause(self, params):
+        sp.set_type(params, sp.TBool)
+        sp.verify(self.is_administrator(sp.sender))
+        self.data.paused = params
+
+class Token_X(Token_X_mint_burn, Token_X_administrator, Token_X_pause, Token_X_core):
+    def __init__(self, admin):
+        Token_X_core.__init__(self, paused = False, administrator = admin)
+
+class Viewer(sp.Contract):
+    def __init__(self, t):
+        self.init(last = sp.none)
+        self.init_type(sp.TRecord(last = sp.TOption(t)))
+    @sp.entry_point
+    def target(self, params):
+        self.data.last = sp.some(params)
+
+
+class Token_Y_core(sp.Contract):
+    def __init__(self, **extra_storage):
+        self.init(balances = sp.big_map(tvalue = sp.TRecord(approvals = sp.TMap(sp.TAddress, sp.TNat), balance = sp.TNat)), totalSupply = 0, **extra_storage)
+
+    @sp.entry_point
+    def transfer(self, params):
+        sp.set_type(params, sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, value = sp.TNat).layout(("from_ as from", ("to_ as to", "value"))))
+        sp.verify(self.is_administrator(sp.sender) |
+            (~self.is_paused() &
+                ((params.from_ == sp.sender) |
+                 (self.data.balances[params.from_].approvals[sp.sender] >= params.value))))
+        self.addAddressIfNecessary(params.to_)
+        sp.verify(self.data.balances[params.from_].balance >= params.value)
+        self.data.balances[params.from_].balance = sp.as_nat(self.data.balances[params.from_].balance - params.value)
+        self.data.balances[params.to_].balance += params.value
+        sp.if (params.from_ != sp.sender) & (~self.is_administrator(sp.sender)):
+            self.data.balances[params.from_].approvals[sp.sender] = sp.as_nat(self.data.balances[params.from_].approvals[sp.sender] - params.value)
+
+    @sp.entry_point
+    def approve(self, params):
+        sp.set_type(params, sp.TRecord(spender = sp.TAddress, value = sp.TNat).layout(("spender", "value")))
+        sp.verify(~self.is_paused())
+        alreadyApproved = self.data.balances[sp.sender].approvals.get(params.spender, 0)
+        sp.verify((alreadyApproved == 0) | (params.value == 0), "UnsafeAllowanceChange")
+        self.data.balances[sp.sender].approvals[params.spender] = params.value
+
+    def addAddressIfNecessary(self, address):
+        sp.if ~ self.data.balances.contains(address):
+            self.data.balances[address] = sp.record(balance = 0, approvals = {})
+
+    @sp.view(sp.TNat)
+    def getBalance(self, params):
+        sp.result(self.data.balances[params].balance)
+
+    @sp.view(sp.TNat)
+    def getAllowance(self, params):
+        sp.result(self.data.balances[params.owner].approvals[params.spender])
+
+    @sp.view(sp.TNat)
+    def getTotalSupply(self, params):
+        sp.set_type(params, sp.TUnit)
+        sp.result(self.data.totalSupply)
+
+    # this is not part of the standard but can be supported through inheritance.
+    def is_paused(self):
+        return sp.bool(False)
+
+    # this is not part of the standard but can be supported through inheritance.
+    def is_administrator(self, sender):
+        return sp.bool(False)
+
+class Token_Y_mint_burn(Token_Y_core):
+    @sp.entry_point
+    def mint(self, params):
+        sp.set_type(params, sp.TRecord(address = sp.TAddress, value = sp.TNat))
+        sp.verify(self.is_administrator(sp.sender))
+        self.addAddressIfNecessary(params.address)
+        self.data.balances[params.address].balance += params.value
+        self.data.totalSupply += params.value
+
+    @sp.entry_point
+    def burn(self, params):
+        sp.set_type(params, sp.TRecord(address = sp.TAddress, value = sp.TNat))
+        sp.verify(self.is_administrator(sp.sender))
+        sp.verify(self.data.balances[params.address].balance >= params.value)
+        self.data.balances[params.address].balance = sp.as_nat(self.data.balances[params.address].balance - params.value)
+        self.data.totalSupply = sp.as_nat(self.data.totalSupply - params.value)
+
+class Token_Y_administrator(Token_Y_core):
+    def is_administrator(self, sender):
+        return sender == self.data.administrator
+
+    @sp.entry_point
+    def setAdministrator(self, params):
+        sp.set_type(params, sp.TAddress)
+        sp.verify(self.is_administrator(sp.sender))
+        self.data.administrator = params
+
+    @sp.view(sp.TAddress)
+    def getAdministrator(self, params):
+        sp.set_type(params, sp.TUnit)
+        sp.result(self.data.administrator)
+
+class Token_Y_pause(Token_Y_core):
+    def is_paused(self):
+        return self.data.paused
+
+    @sp.entry_point
+    def setPause(self, params):
+        sp.set_type(params, sp.TBool)
+        sp.verify(self.is_administrator(sp.sender))
+        self.data.paused = params
+
+class Token_Y(Token_Y_mint_burn, Token_Y_administrator, Token_Y_pause, Token_Y_core):
+    def __init__(self, admin):
+        Token_Y_core.__init__(self, paused = False, administrator = admin)
+
+class Viewer(sp.Contract):
+    def __init__(self, t):
+        self.init(last = sp.none)
+        self.init_type(sp.TRecord(last = sp.TOption(t)))
+    @sp.entry_point
+    def target(self, params):
+        self.data.last = sp.some(params)
 
 class TokenSwapper(sp.Contract):
     def __init__(self, Token_X, Token_Y):
-        self.init(Token_X  = Token_X,
-                  Token_Y   = Token_Y,
-                  counter = 0)
+        self.init(Token_X  = Token_X, Token_Y = Token_Y, counter = 0)
+
 #value is x
     @sp.entry_point
-    def swap_token(self, x):
-        tk = sp.TRecord(k = sp.TContract(sp.TNat), x = sp.TNat)
-        params = sp.record(k = sp.self_entry_point("burn"), x = x)
-        call(sp.contract(tk, self.data.Token_X).open_some(), params)
+    def swapper(self, x, admin_addr, user_addr):
+        sp.sender = admin_addr
+        contractParams = sp.contract(sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, value = sp.TNat).layout(("from_ as from", ("to_ as to", "value"))), self.data.Token_X, entry_point="transfer").open_some()
+        dataToBeSent = sp.record(from_ = user_addr, to_ = admin_addr, value = x)
+        sp.transfer(dataToBeSent, sp.mutez(0), contractParams)
         
-        next_param = sp.record(k = sp.self_entry_point("mint"), x = x) 
+        contractParams2 = sp.contract(sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, value = sp.TNat).layout(("from_ as from", ("to_ as to", "value"))), self.data.Token_Y, entry_point="transfer").open_some()
+        dataToBeSent2 = sp.record(from_ = admin_addr, to_ = user_addr, value = x)
+        sp.transfer(dataToBeSent2, sp.mutez(0), contractParams2)
+        
+        
+        
+        
+        """
+        contractParams2 = sp.contract(sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, value = sp.TNat), self.data.Token_Y, entry_point="transfer").open_some()
+        dataToBeSent2 = sp.record(from_ = admin_addr, to_ = user_addr, value = x)
+        sp.transfer(dataToBeSent2,sp.mutez(0),contractParams2)
+        """
+        """
+        t = sp.TRecord(owner    = sp.TAddress,
+        operator = sp.TAddress,
+        token_id = sp.TString)
+        t_with_layout = t.layout(("owner", ("operator", "token_id"))
+        next_param = sp.record(from_ = user_addr, to_ = admin_addr, value = x, k = sp.self_entry_point("transfer"))
         call(sp.contract(tk, self.data.Token_Y).open_some(), next_param)
-        
-        
-
-    @sp.entry_point
-    def reset(self, params):
-        self.data.counter = 0
-
-
-
-@sp.add_test(name = "Swapper")
-def test():
-    scenario = sp.test_scenario()
+"""
     
-    token_x = Token_X()
-    scenario += token_x
-    token_y = Token_Y()
-    scenario += token_y
-    swapper = TokenSwapper(Token_X = token_x.address,
-                      Token_Y  = token_y.address)
-    scenario += swapper
-    
+if "templates" not in __name__:
+    @sp.add_test(name = "Tokens")
+    def test():
 
+        scenario = sp.test_scenario()
+        scenario.h1("tokens test")
+
+        scenario.table_of_contents()
+
+        # sp.test_account generates ED25519 key-pairs deterministically:
+        admin = sp.test_account("Administrator")
+        alice = sp.test_account("Alice")
+        bob   = sp.test_account("Robert")
+
+        # Let's display the accounts:
+        scenario.h1("Accounts")
+        scenario.show([admin, alice, bob])
+
+        scenario.h1("Contract")
+        c1 = Token_X(admin.address)
+        c2 = Token_Y(admin.address)
+        c3 = TokenSwapper(Token_X = c1.address, Token_Y = c2.address)
+        scenario.h1("Entry points")
+        scenario += c1
+        scenario += c2
+        scenario += c3
+        scenario.h2("Admin mints a few coins")
+        scenario += c1.mint(address = alice.address, value = 18).run(sender = admin)
+        
+        scenario += c2.mint(address = alice.address, value = 10).run(sender = admin)
+        scenario += c2.mint(address = admin.address, value = 100).run(sender = admin)
+        scenario += c2.mint(address = bob.address, value = 10).run(sender = admin)
+        scenario.verify(c2.data.balances[alice.address].balance == 10)
+        
+        scenario.h2("Alice transfers to Bob")
+        scenario += c1.transfer(from_ = alice.address, to_ = bob.address, value = 4).run(sender = alice)
+        scenario.verify(c1.data.balances[alice.address].balance == 14)
+        scenario.h2("Bob tries to transfer from Alice but he doesn't have her approval")
+        scenario += c1.transfer(from_ = alice.address, to_ = bob.address, value = 4).run(sender = bob, valid = False)
+        scenario.h2("Alice approves Bob and Bob transfers")
+        scenario += c1.approve(spender = bob.address, value = 5).run(sender = alice)
+        scenario += c1.transfer(from_ = alice.address, to_ = bob.address, value = 4).run(sender = bob)
+        scenario.h2("Bob tries to over-transfer from Alice")
+        scenario += c1.transfer(from_ = alice.address, to_ = bob.address, value = 4).run(sender = bob, valid = False)
+        scenario.h2("Admin burns Bob token")
+        scenario += c1.burn(address = bob.address, value = 1).run(sender = admin)
+        scenario.verify(c1.data.balances[alice.address].balance == 10)
+        scenario.h2("Alice tries to burn Bob token")
+        scenario += c1.burn(address = bob.address, value = 1).run(sender = alice, valid = False)
+        scenario.h2("Admin pauses the contract and Alice cannot transfer anymore")
+        scenario += c1.setPause(True).run(sender = admin)
+        scenario += c1.transfer(from_ = alice.address, to_ = bob.address, value = 4).run(sender = alice, valid = False)
+        scenario.verify(c1.data.balances[alice.address].balance == 10)
+        scenario.h2("Admin transfers while on pause")
+        scenario += c1.transfer(from_ = alice.address, to_ = bob.address, value = 1).run(sender = admin)
+        scenario.h2("Admin unpauses the contract and transferts are allowed")
+        scenario += c1.setPause(False).run(sender = admin)
+        scenario.verify(c1.data.balances[alice.address].balance == 9)
+        scenario += c1.transfer(from_ = alice.address, to_ = bob.address, value = 1).run(sender = alice)
+        
+        scenario.h2("Administrator")
+        view_administrator = Viewer(sp.TAddress)
+        scenario += view_administrator
+        scenario += c1.getAdministrator((sp.unit, view_administrator.typed))
+        scenario.verify_equal(view_administrator.data.last, sp.some(admin.address))
+
+        scenario += c3.swapper(x = 5, admin_addr = admin.address, user_addr = alice.address).run(sender = admin)
+        #scenario.verify(c1.data.balances[alice.address].balance == 3)
+        #scenario.verify(c2.data.balances[alice.address].balance == 15)
+        
